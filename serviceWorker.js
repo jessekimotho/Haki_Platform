0f2e29add41f7d95aec0a1a0c32620a9373009f9
goodlife <christopherndugo@gmail.com>
2020-08-24 14:56:28 +0300

    update

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,7 @@
 var CACHE_NAME = 'static-cache';
 var urlsToCache = [
-    '.',
-    'index.html',
+    ".",
+    "index.html",
     "style.css",
     "app.js",
 ];
@@ -10,38 +10,38 @@ let deferredPrompt;
 
 
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
-        .then(function(cache) {
-            return cache.addAll(urlsToCache);
-        })
+            .then(function (cache) {
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request)
-        .then(function(response) {
-            return response || fetchAndCache(event.request);
-        })
+            .then(function (response) {
+                return response || fetchAndCache(event.request);
+            })
     );
 });
 
 function fetchAndCache(url) {
     return fetch(url)
-        .then(function(response) {
+        .then(function (response) {
             // Check if we received a valid response
             if (!response.ok) {
                 throw Error(response.statusText);
             }
             return caches.open(CACHE_NAME)
-                .then(function(cache) {
+                .then(function (cache) {
                     cache.put(url, response.clone());
                     return response;
                 });
         })
-        .catch(function(error) {
+        .catch(function (error) {
             console.log('Request failed:', error);
             // You could return a custom offline 404 page here
         });
